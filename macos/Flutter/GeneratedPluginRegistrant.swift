105a101e233bf96fb87c2027ac72da46de98f214
Jaeeun Jung <rlt231022@gmail.com>
2024-06-10 02:37:46 +0900

    오류해결중

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import shared_preferences_foundation
 import firebase_core
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
