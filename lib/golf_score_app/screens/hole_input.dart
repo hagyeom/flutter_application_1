8b45fc4d0d3bf8d3c274216075f2b96ef9f34601
ByunChoeun <quschdms25@gmail.com>
2024-06-19 08:09:24 +0900

    표 꾸미기

--- lib/golf_score_app/screens/hole_input.dart
+++ lib/golf_score_app/screens/hole_input.dart
@@ -3,12 +3,12 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'score_card.dart';
 
-
 List<int> pars = List.filled(18, 4); // 초기값은 모두 4로 임시 설정
 List<List<int>> scores = List.generate(4, (_) => List.filled(18, 0));
 List<bool> holeCompleted = List.filled(18, false); // 각 홀의 입력 완료 상태를 추적하는 리스트
-List<List<Color>> teamColors = List.generate(18, (_) => List.filled(4, Colors.white)); // 각 홀의 팀 색상을 추적하는 리스트
-int teamType=4;
+List<List<Color>> teamColors = List.generate(
+    18, (_) => List.filled(4, Colors.white)); // 각 홀의 팀 색상을 추적하는 리스트
+int teamType = 4;
 
 class HoleInput extends StatefulWidget {
   final int holeIndex;
@@ -28,7 +28,7 @@ class _HoleInputState extends State<HoleInput> {
     super.initState();
     scoreControllers = List.generate(
       4,
-          (playerIndex) => TextEditingController(text: ''),
+      (playerIndex) => TextEditingController(text: ''),
     );
   }
 
@@ -44,13 +44,16 @@ class _HoleInputState extends State<HoleInput> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MakeAppBar.build(context),
+      backgroundColor: Color(0xFFEFEEEE), // 앱 내부 배경색 설정
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             Text('현재 홀은 ${widget.holeIndex + 1}홀 입니다.'),
             Text('현재 홀의 타수를 입력하세요. '),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Expanded(
               child: Table(
                 border: TableBorder.all(),
@@ -62,18 +65,28 @@ class _HoleInputState extends State<HoleInput> {
                   TableRow(
                     children: [
                       TableCell(
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('선수', style: TextStyle(fontWeight: FontWeight.bold)),
+                        child: Container(
+                          color: Color(0xFFDBE4CB),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('선수',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ),
                           ),
                         ),
                       ),
                       TableCell(
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('타수', style: TextStyle(fontWeight: FontWeight.bold)),
+                        child: Container(
+                          color: Color(0xFFDBE4CB),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('타수',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ),
                           ),
                         ),
                       ),
@@ -83,21 +96,27 @@ class _HoleInputState extends State<HoleInput> {
                     TableRow(
                       children: [
                         TableCell(
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(players[i]),
+                          child: Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(players[i]),
+                              ),
                             ),
                           ),
                         ),
                         TableCell(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextField(
-                              controller: scoreControllers[i],
-                              keyboardType: TextInputType.number,
-                              decoration: InputDecoration(
-                                border: InputBorder.none, // Border removed
+                          child: Container(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: scoreControllers[i],
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none, // Border removed
+                                ),
                               ),
                             ),
                           ),
@@ -110,44 +129,50 @@ class _HoleInputState extends State<HoleInput> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    teamType = 0;
-                  });
-                  saveScores();
-                  assignTeams(); // 팀 지정 및 결과 색상 적용
-                  navigateToNextHole(context);
-                },
-                child: Text('개인전'),
-              ),
-              SizedBox(width: 10,),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    teamType = 1;
-                  });
-                  saveScores();
-                  assignTeams(); // 팀 지정 및 결과 색상 적용
-                  navigateToNextHole(context);
-                },
-                child: Text('2:2'),
-              ),
-                SizedBox(width: 10,),
                 ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    teamType = 2;
-                  });
-                  saveScores();
-                  assignTeams(); // 팀 지정 및 결과 색상 적용
-                  navigateToNextHole(context);
-                },
-                child: Text('3:1'),
-              ),
-            ],
+                  onPressed: () {
+                    setState(() {
+                      teamType = 0;
+                    });
+                    saveScores();
+                    assignTeams(); // 팀 지정 및 결과 색상 적용
+                    navigateToNextHole(context);
+                  },
+                  child: Text('개인전'),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      teamType = 1;
+                    });
+                    saveScores();
+                    assignTeams(); // 팀 지정 및 결과 색상 적용
+                    navigateToNextHole(context);
+                  },
+                  child: Text('2:2'),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      teamType = 2;
+                    });
+                    saveScores();
+                    assignTeams(); // 팀 지정 및 결과 색상 적용
+                    navigateToNextHole(context);
+                  },
+                  child: Text('3:1'),
+                ),
+              ],
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             ElevatedButton(
               onPressed: () {
                 setState(() {
@@ -159,7 +184,9 @@ class _HoleInputState extends State<HoleInput> {
               },
               child: Text(widget.holeIndex < 17 ? '팀 랜덤 기록' : '경기 종료'),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -178,7 +205,8 @@ class _HoleInputState extends State<HoleInput> {
   void saveScores() {
     setState(() {
       for (int i = 0; i < players.length; i++) {
-        scores[i][widget.holeIndex] = int.tryParse(scoreControllers[i].text) ?? 0;
+        scores[i][widget.holeIndex] =
+            int.tryParse(scoreControllers[i].text) ?? 0;
       }
       holeCompleted[widget.holeIndex] = true;
     });
@@ -189,41 +217,44 @@ class _HoleInputState extends State<HoleInput> {
     List<Color> teamColorsForHole = List.filled(4, Colors.white);
     List<int> indices = [0, 1, 2, 3];
     indices.shuffle();
-    if(teamType==4)teamType = Random().nextInt(3); // 0: 개인, 1: 2대2, 2: 3대1
+    if (teamType == 4) teamType = Random().nextInt(3); // 0: 개인, 1: 2대2, 2: 3대1
 
     if (teamType == 0) {
       // 개인전
-      int winnerIndex = indices.reduce((a, b) => scores[a][widget.holeIndex] < scores[b][widget.holeIndex] ? a : b);
-      if(scores[indices[0]][widget.holeIndex] == scores[indices[1]][widget.holeIndex] &&
-      scores[indices[0]][widget.holeIndex] == scores[indices[2]][widget.holeIndex] &&
-      scores[indices[0]][widget.holeIndex] == scores[indices[3]][widget.holeIndex]){
-        for(int i=0;i<4;i++){
+      int winnerIndex = indices.reduce((a, b) =>
+          scores[a][widget.holeIndex] < scores[b][widget.holeIndex] ? a : b);
+      if (scores[indices[0]][widget.holeIndex] ==
+              scores[indices[1]][widget.holeIndex] &&
+          scores[indices[0]][widget.holeIndex] ==
+              scores[indices[2]][widget.holeIndex] &&
+          scores[indices[0]][widget.holeIndex] ==
+              scores[indices[3]][widget.holeIndex]) {
+        for (int i = 0; i < 4; i++) {
           teamColorsForHole[indices[i]] = Colors.green;
         }
-      }
-      else{
-      for (int i = 0; i < 4; i++) {
-        teamColorsForHole[indices[i]] = (i == winnerIndex) ? Colors.red : Colors.white;
+      } else {
+        for (int i = 0; i < 4; i++) {
+          teamColorsForHole[indices[i]] =
+              (i == winnerIndex) ? Colors.red : Colors.white;
         }
       }
     } else if (teamType == 1) {
       // 2대2 팀
-      int team1Score = scores[indices[0]][widget.holeIndex] + scores[indices[1]][widget.holeIndex];
-      int team2Score = scores[indices[2]][widget.holeIndex] + scores[indices[3]][widget.holeIndex];
+      int team1Score = scores[indices[0]][widget.holeIndex] +
+          scores[indices[1]][widget.holeIndex];
+      int team2Score = scores[indices[2]][widget.holeIndex] +
+          scores[indices[3]][widget.holeIndex];
       if (team1Score < team2Score) {
         teamColorsForHole[indices[0]] = Colors.red;
         teamColorsForHole[indices[1]] = Colors.red;
         teamColorsForHole[indices[2]] = Colors.blue;
         teamColorsForHole[indices[3]] = Colors.blue;
-      }
-      else if (team1Score == team2Score) {
+      } else if (team1Score == team2Score) {
         teamColorsForHole[indices[0]] = Colors.lightGreen;
         teamColorsForHole[indices[1]] = Colors.lightGreen;
         teamColorsForHole[indices[2]] = Colors.green;
         teamColorsForHole[indices[3]] = Colors.green;
-      }
-
-      else {
+      } else {
         teamColorsForHole[indices[0]] = Colors.blue;
         teamColorsForHole[indices[1]] = Colors.blue;
         teamColorsForHole[indices[2]] = Colors.red;
@@ -231,7 +262,11 @@ class _HoleInputState extends State<HoleInput> {
       }
     } else if (teamType == 2) {
       // 3대1 팀
-      List<int> team3Scores = [scores[indices[0]][widget.holeIndex], scores[indices[1]][widget.holeIndex], scores[indices[2]][widget.holeIndex]];
+      List<int> team3Scores = [
+        scores[indices[0]][widget.holeIndex],
+        scores[indices[1]][widget.holeIndex],
+        scores[indices[2]][widget.holeIndex]
+      ];
       team3Scores.sort();
       int team3Score = team3Scores[0] + team3Scores[1];
       int team1Score = scores[indices[3]][widget.holeIndex] * 2;
@@ -240,14 +275,12 @@ class _HoleInputState extends State<HoleInput> {
         teamColorsForHole[indices[1]] = Colors.red;
         teamColorsForHole[indices[2]] = Colors.red;
         teamColorsForHole[indices[3]] = Colors.blue;
-      }
-      else if (team3Score == team1Score) {
+      } else if (team3Score == team1Score) {
         teamColorsForHole[indices[0]] = Colors.green;
         teamColorsForHole[indices[1]] = Colors.green;
         teamColorsForHole[indices[2]] = Colors.green;
         teamColorsForHole[indices[3]] = Colors.lightGreen;
-      }
-      else {
+      } else {
         teamColorsForHole[indices[0]] = Colors.blue;
         teamColorsForHole[indices[1]] = Colors.blue;
         teamColorsForHole[indices[2]] = Colors.blue;
@@ -266,7 +299,8 @@ class _HoleInputState extends State<HoleInput> {
     if (nextHoleIndex < 18) {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HoleInput(holeIndex: nextHoleIndex)),
+        MaterialPageRoute(
+            builder: (context) => HoleInput(holeIndex: nextHoleIndex)),
       );
     } else {
       Navigator.pushReplacement(
